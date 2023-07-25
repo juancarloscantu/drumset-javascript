7e28eb03dd102c182a37df3f1c77171151547156
Jc Cantu <72985444+juancarloscantu@users.noreply.github.com>
2023-07-24 19:57:23 -0500

    Add files via upload
    added animaiton

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
@@ -13,6 +15,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -55,4 +59,14 @@ function makeSound(key) {
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
